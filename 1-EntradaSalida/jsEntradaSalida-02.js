8820ecd7adfa1b1133a3ef06206ec611512b7178
Jonathanmaciel89 <jonmaciel@hotmail.com>
2020-07-13 13:11:26 -0300

    carga de ejercicios 1 a 5

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,18 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+	nombre = prompt ("Ingresa tu nombre", "Ernesto Jonathan Maciel");
+	alert("el nombre ingresado es:" +nombre);
+
+	//alert (nombre)
+
+	//test
+	/* var nombre; --> generar variable
+	nombre = prompt ("Ingresa tu nombre"; "nombre x")
+
+	EL PROMPT SIEMPRE SE ESCRIBE CON SIGNO IGUAL ANTES
+
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,19 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var edadIngresado;
+	var nombreIngresado;
+
+	nombreIngresado=txtIdNombre.value;
+	edadIngresada=txtIdEdad.value;
+// primer paso concatenar
+	alert("Usted se llama "+nombreIngresado );
+	alert("y tiene " +edadIngresada + " años");
+
+	// segundo paso
+	alert("Usted se llama " +nombreIngresado+ " y tiene " +edadIngresada + " años");
+
 }
 
+//declaro variable
+//asigno valores: es la entrada de datos a traves de prompt, Id, etc.
